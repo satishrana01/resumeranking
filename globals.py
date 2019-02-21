b979d17e5884b5461404eae6ef02cdd3b13944b5
satish kumar <satish.kumar2@evry.com>
2019-02-21 09:01:20 +0530

    some more error fix

--- globals.py
+++ globals.py
@@ -1,6 +1,11 @@
 import os
+import nltk
 
 def initialize():
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('maxent_ne_chunker')
+    nltk.download('words')
     global rootpath
     rootpath = os.getcwd()
     global pathSeprator
